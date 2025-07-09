15a2406cb47a18a803110c87b8516d403d814d56
KazumaWada <kazumawadaa@gmail.com>
2025-07-09 13:36:41 +0900

    complete contact form 04

---
+++ practice-1/src/main/java/com/example/demo/service/ContactServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import com.example.demo.form.ContactForm;
+
+public class ContactServiceImpl implements ContactService {
+
+	@Override
+	public void saveContact(ContactForm contactForm) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
